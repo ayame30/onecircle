6760955a281e301ffa5a4a1c08a1f4090d8c5c4f
ayame30 <alnaym30@gmail.com>
2017-01-17 00:11:31 +0800

    Update header

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(function () { // wait for document ready
 		var controller = new ScrollMagic.Controller();
     var tween = new TimelineMax()
 			.add(TweenMax.to($("#intro-text-1"), 1, {css:{opacity: 0}, ease:Power1.easeInOut}))
+			.add(TweenMax.to($("#logo-2"), 1, {css:{opacity: 1}, ease:Power1.easeInOut}))
+			.add(TweenMax.to($("#logo-1"), 1, {css:{opacity: 0}, ease:Power1.easeInOut}))
       .add(TweenMax.to($("#intro-text-2"), 1, {css:{opacity: 1}, ease:Power1.easeInOut}))
       .add(TweenMax.to($("#intro-text-2"), 1, {css:{opacity: 0}, ease:Power1.easeInOut}))
       .add(TweenMax.to($("#intro-text-3"), 1, {css:{opacity: 1}, ease:Power1.easeInOut}));
@@ -18,22 +20,26 @@ $(function () { // wait for document ready
       .add(TweenMax.to($("header nav a:not(.first)"), 0.2, {css:{opacity:0, y: -50}, ease:Power1.easeInOut}))
       .add(TweenMax.to($("header nav a:not(.first)"), 0.2, {css:{y: 0, x:-50}, ease:Power1.easeInOut}))
       .add(TweenMax.to($("header nav a"), 0.1, {css:{width: "15%", marginLeft: -18}, ease:Power1.easeInOut}))
-      .add(TweenMax.to($("header nav a:not(.first)"), 0.2, {css:{x: 0, opacity:1}, ease:Power1.easeInOut}));
+      .add(TweenMax.to($("header nav a:not(.first)"), 0.2, {css:{x: 0, opacity:1}, ease:Power1.easeInOut}))
+	  	  	  .add(TweenMax.to($(".fixed-container"), 0, {css:{opacity: 1}, ease:null}));
     var headerScene = new ScrollMagic.Scene({triggerElement: ".header-space",  triggerHook: 0})
 					.setPin("header")
-          .setTween(headerTween)
+          			.setTween(headerTween)
 					//.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
-		var sectionATween = new TimelineMax()
-      .add(TweenMax.to($("#section-content"), 0.1, {css:{hight: "50%"}, ease:Power1.easeInOut}));
-    var section1Scene = new ScrollMagic.Scene({triggerElement: ".section-a",  triggerHook: 0})
-					.setPin("#section-content")
-          .setTween(sectionATween)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+
+
 		var footerScene = new ScrollMagic.Scene({triggerElement: ".footer",  triggerHook: 1, offset: 50})
 					.setPin(".footer")
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
+
+	$(document).on('scroll', function() {
+		if($('.section-b').offset().top < $(window).scrollTop() + $(window).height()/2) {
+			$('#section-content').removeClass('a');
+			$('#section-content').empty();
+
+		}
+	})
 
 });
